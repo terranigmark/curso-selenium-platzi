25a2e607b7c87e2aaf872a08897936445331ae0d
Héctor Vega <linnk99@gmail.com>
2020-05-17 21:28:20 -0500

    Archivos de clase 'Ordenar tablas'
    - Archivos generados:
    tables.py

---
+++ tables.py
@@ -0,0 +1,33 @@
+import unittest
+from selenium import webdriver
+from time import sleep
+
+class Tables(unittest.TestCase):
+
+    def setUp(self):
+        self.driver = webdriver.Chrome(executable_path = "./chromedriver")
+        driver = self.driver
+        driver.get("http://the-internet.herokuapp.com")
+        driver.find_element_by_link_text("Sortable Data Tables").click()
+
+    def test_sort_tables(self):
+        driver = self.driver
+
+        table_data = [[] for i in range(5)]
+        print(table_data)
+
+        for i in range(5):
+            header = driver.find_element_by_xpath(f'//*[@id="table1"]/thead/tr/th[{i + 1}]/span')
+            table_data[i].append(header.text)
+
+            for j in range(4):
+                row_data = driver.find_element_by_xpath(f'//*[@id="table1"]/tbody/tr[{j + 1}]/td[{j + 1}]')
+                table_data[i].append(row_data.text)
+
+        print(table_data)
+
+    def tearDown(self):
+        self.driver.close()
+
+if __name__ == "__main__":
+    unittest.main()
